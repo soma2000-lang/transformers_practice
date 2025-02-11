12546b707b93af7dc268b2cd4b0e2a2596045995
Somasree Majumder <bishnupadamajumder32@gmail.com>
2025-02-11 08:10:42 +0530

    Update and rename autodecode.py to Automatically decode webdataset sample.py

--- Automatically decode webdataset sample.py
+++ Automatically decode webdataset sample.py
@@ -1,8 +1,4 @@
-#
-# Copyright (c) 2017-2021 NVIDIA CORPORATION. All rights reserved.
-# This file is part of the WebDataset library.
-# See the LICENSE file for licensing terms (BSD-style).
-#
+
 
 """Automatically decode webdataset samples."""
 
